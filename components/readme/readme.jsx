17929e1a2fa7aaad318b6d9f13910a2b1094045f
Pierre Berger <pierre.berger@wizbii.com>
2021-12-02 13:59:54 +0100

    update dependencies

--- components/readme/readme.jsx
+++ components/readme/readme.jsx
@@ -1,9 +1,9 @@
-import marked from 'marked';
+import { marked } from 'marked';
 import sanitizeHtml from 'sanitize-html';
 import styles from './readme.module.css';
 
 export default function ReadMe({content}) {
-    const rawMarkup = {__html: sanitizeHtml(marked(content))};
+    const rawMarkup = {__html: sanitizeHtml(marked.parse(content))};
 
     return (
         <div className={styles.readme} dangerouslySetInnerHTML={rawMarkup} />
