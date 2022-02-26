06d37dcd68bad5712065e2fb8fd8f2c26e7f61d4
metzgegu <guillaume.metzger@swile.co>
2022-02-26 19:21:23 +0100

    Add social-links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,14 @@
     import Header from '../components/header.svelte';
     import infos from '../../static/data/info.json';
     import "../app.css";
+    import SocialLinks from '../components/social-links.svelte';
 </script>
 
 <Header title={infos.name} job={infos.job}/>
+
+<SocialLinks
+    github={infos.github_link}
+    linkedin={infos.linkedin_link}
+    twitter={infos.twitter_link}
+    email={infos.email_link}
+/>
