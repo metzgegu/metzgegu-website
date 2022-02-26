521369bf5dec0935df8569836e00639860f331e7
metzgegu <guillaume.metzger@swile.co>
2022-02-26 22:34:59 +0100

    Disable tab in prettier

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,8 +1,8 @@
 <script>
-	export let title, job;
+  export let title, job;
 </script>
 
 <section class="text-center">
-	<h1 class="font-bold text-7xl mb-12">{title}</h1>
-	<h2 class="my-4 text-4xl">{job}</h2>
+  <h1 class="font-bold text-7xl mb-12">{title}</h1>
+  <h2 class="my-4 text-4xl">{job}</h2>
 </section>
--- utils/action.async.js
+++ utils/action.async.js
@@ -1,9 +1,9 @@
 export async function getGitHubLabContributions(token, username) {
-	const headers = {
-		Authorization: `bearer ${token}`
-	};
-	const body = {
-		query: `query {
+  const headers = {
+    Authorization: `bearer ${token}`
+  };
+  const body = {
+    query: `query {
             user(login: "${username}") {
               name
               contributionsCollection {
@@ -23,27 +23,27 @@ export async function getGitHubLabContributions(token, username) {
               }
             }
           }`
-	};
-	const gitHubResponse = await fetch('https://api.github.com/graphql', {
-		method: 'POST',
-		body: JSON.stringify(body),
-		headers: headers
-	});
-	const gitLabResponse = await fetch(`https://gitlab.com/users/${username}/calendar.json`);
-	const gitLabData = await gitLabResponse.json();
-	const gitHubData = await gitHubResponse.json();
-	if (!gitHubData?.data?.user?.contributionsCollection?.contributionCalendar?.weeks) {
-		return { values: [], moy: 0, max: 0 };
-	}
-	const values = gitHubData.data.user.contributionsCollection.contributionCalendar.weeks
-		.flatMap((item) => item.contributionDays)
-		.map((item) => ({
-			date: item.date,
-			count: item.contributionCount + (gitLabData[item.date] ? gitLabData[item.date] : 0)
-		}));
-	const valuesWithoutEmpty = values.filter((val) => val.count !== 0);
-	const moy =
-		valuesWithoutEmpty.reduce((acc, val) => acc + val.count, 0) / valuesWithoutEmpty.length;
-	const max = values.reduce((max, value) => Math.max(max, value.count), 0);
-	return { values, moy, max };
+  };
+  const gitHubResponse = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: headers
+  });
+  const gitLabResponse = await fetch(`https://gitlab.com/users/${username}/calendar.json`);
+  const gitLabData = await gitLabResponse.json();
+  const gitHubData = await gitHubResponse.json();
+  if (!gitHubData?.data?.user?.contributionsCollection?.contributionCalendar?.weeks) {
+    return { values: [], moy: 0, max: 0 };
+  }
+  const values = gitHubData.data.user.contributionsCollection.contributionCalendar.weeks
+    .flatMap((item) => item.contributionDays)
+    .map((item) => ({
+      date: item.date,
+      count: item.contributionCount + (gitLabData[item.date] ? gitLabData[item.date] : 0)
+    }));
+  const valuesWithoutEmpty = values.filter((val) => val.count !== 0);
+  const moy =
+    valuesWithoutEmpty.reduce((acc, val) => acc + val.count, 0) / valuesWithoutEmpty.length;
+  const max = values.reduce((max, value) => Math.max(max, value.count), 0);
+  return { values, moy, max };
 }
